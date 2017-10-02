2c3461db57b8b3d5b8cc6d06e90bc443f735f7c2
summer1168 <demoncr@vip.qq.com>
2017-10-02 20:24:35 +0800

    lab

---
+++ chocolate/src/chocolate/test.java
@@ -0,0 +1,17 @@
+package chocolate;
+
+public class test {
+	 public static void main(String[] args){
+		   chocolate a=new chocolate();
+		   chocolate b=new chocolate();
+		   a.fill();
+		   a.fill();
+		   a.boil();
+		   a.boil();
+		   a.drain();
+		   a.drain();
+		   
+		   
+	   }
+}
+
